fea9b4b289327634fd80e55d645db1896e88fd88
DESKTOP-NQDA7TS\ThanhTu <tuthanh375@gmail.com>
2024-01-21 22:20:37 +0700

    first commit

---
+++ src/main/java/vn/com/edu/fit/tool/ParserTool.java
@@ -0,0 +1,45 @@
+package vn.com.edu.fit.tool;
+
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class ParserTool {
+    private void getPackage(File filePath) throws FileNotFoundException {
+        CompilationUnit cu = StaticJavaParser.parse(filePath);
+        List<PackageDeclaration> packages = cu.findAll(PackageDeclaration.class);
+        packages.forEach(System.out::println);
+    }
+
+    private void getAllMethods(File filePath) throws FileNotFoundException {
+        CompilationUnit cu = StaticJavaParser.parse(filePath);
+        List<MethodDeclaration> methods = cu.findAll(MethodDeclaration.class);
+        for (MethodDeclaration method: methods){
+            System.out.println(
+                    method.getType() + " " + method.getName()
+            );
+        }
+    }
+
+    private void getAllField(File filePath) throws FileNotFoundException {
+        CompilationUnit cu = StaticJavaParser.parse(filePath);
+        List<FieldDeclaration> fields = cu.findAll(FieldDeclaration.class);
+        fields.forEach(System.out::println);
+        }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ParserTool parserTool = new ParserTool();
+        File file = new File("D:\\Architecture (JavaParser)\\ParserTool\\src\\main\\java\\vn\\com\\edu\\fit\\models\\Car.java");
+        parserTool.getAllField(file);
+        parserTool.getAllMethods(file);
+        parserTool.getPackage(file);
+    }
+}
